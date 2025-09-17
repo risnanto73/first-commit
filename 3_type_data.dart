3040346cd0ffd9e957e8867ea4f781e8de52551a
risnanto73 <tiolast63@gmail.com>
2025-09-17 14:27:36 +0700

    belajar tipe data

---
+++ 3_type_data.dart
@@ -0,0 +1,65 @@
+/**
+ * variable adalah suatu wadah untuk 
+ * menyimpan data
+ */
+
+/**
+ * Tipe Data adalah suatu konsep yang digunakan
+ * untuk mengklasifikasikan jenis data
+ * yang dapat diproses oleh program
+ */
+
+/**
+ * Jenis - jenis tipe data
+ */
+
+/**
+ * int => integer => bilangan bulat 
+ * => 1, -2, 11 dan sebagainya
+ */
+
+/**
+ * double => bilangan desimal
+ * => 3.14, -0.1, 2.5 dan sebagainya
+ */
+
+/**
+ * num => gabungan dari integer dan double
+ * 3.14, 2 , -2, 0 ,3.15
+ */
+
+/**
+ * bool = boolean = true or false
+ */
+
+/**
+ * String = string = "kumpulan karakter"
+ */
+
+/**
+ * List = berupa daftar
+ * [1, 2, 3, 4, 5, 6, 7, 0, -2]
+ * ['a', 'b', 'k']
+ */
+
+/**
+ * Map => berupa key:value
+ * {"nama_kota" : "Jakarta", "ID" : 62}
+ */
+
+/**
+ * Dynamic = Campuran / Tipe apapun
+ * [1, 'a', {"nama_kota" : "Jakarta"}]
+ */
+
+void main(List<String> args) {
+  String nama = "Ucup"; //string
+  int age = 24; // integer
+
+  var tinggi_badan; //dynamic
+  tinggi_badan = 159;
+  tinggi_badan = "seratus enam puluh";
+
+  bool isMaried = false;
+  double berat_badan = 67.7;
+}
