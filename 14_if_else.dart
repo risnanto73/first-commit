728f1d64e33637e581f41d8a0bb73f54cc4a3d6b
risnanto73 <tiolast63@gmail.com>
2025-09-25 14:17:48 +0700

    challenge if else

--- 14_if_else.dart
+++ 14_if_else.dart
@@ -39,6 +39,19 @@ void main(List<String> args) {
   }
 
   /**
+   * program menggunakan stdin (input) nilaiUjian
+   * varibalenya nilaiUjian type data num
+   * ketentuan nilai hasilnya ada dibawah ini
+   * jika nilai kurang dari 0 maka outputnya
+   * "Tidak terdifinisi (tidak terdaftar)"
+   * * jika nilai lebih dari 100 maka outputnya
+   * "ini adalah cheater"
+   * 
+   * Nilai anda "A/B/C/D/E"
+   * A-C itu lulus dan D-E itu remedial
+   */
+
+  /**
    * Nilai E = <=70   // kurang dari sama dengan 70 nilai (E)
    * Nilai D = 71-75  // 71-75 nilai (D)
    * Nilai C = 76-85  // 76-85 nilai (C)
