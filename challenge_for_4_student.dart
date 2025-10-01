efa8fdb224fcc0916a8fb5c87f05a21957ca4578
risnanto73 <tiolast63@gmail.com>
2025-10-01 11:16:24 +0700

    belajar switch case

---
+++ challenge_for_4_student.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  /**
+   * buatkan perhitungan rumus 
+   * volume dan luas permukaan bola
+   * π = 3.14 
+   * π = 22/7
+   * 
+   * volume = 4/3 * π * r * r * r
+   * luas p = 4 * π * r * r
+   */
+}
