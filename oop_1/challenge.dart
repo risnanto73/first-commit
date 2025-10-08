1198b7915e50a80436b58ec55f6ee32c9ad567fb
risnanto73 <tiolast63@gmail.com>
2025-10-08 13:50:12 +0700

    feat: add Book class implementation with properties, constructor, and methods

---
+++ oop_1/challenge.dart
@@ -0,0 +1,29 @@
+/**
+ * Buatlah sebuah class bernama 
+ * Book yang merepresentasikan buku di toko. 
+ * Setiap buku memiliki:
+ * - Properties:
+ * - title (String)
+ * - author (String)
+ * - price (double)
+ * - stock (int)
+ * 
+ * - Constructor:
+ * - Harus bisa menginisialisasi semua properti.
+ * 
+ * - Method:
+ * discount(double percent) 
+ * → Mengurangi harga berdasarkan persentase.
+ * restock(int amount) 
+ * → Menambah stok.
+ * display() 
+ * → Menampilkan detail buku.
+ * 
+ * Tantangan Utama
+ * Buat class Book sesuai spesifikasi.
+ * Buat 2 objek buku menggunakan constructor.
+ * Gunakan cascade notation untuk:
+ * Memberi diskon
+ * Menambah stok
+ * Menampilkan detail buku
+ */
